7ab17b70d689e16512308df842fe88a6aa9a285d
Lonnen <chris.lonnen@gmail.com>
2012-07-25 18:26:33 -0700

    serve a page with ace running

---
+++ datazine.py
@@ -0,0 +1,14 @@
+"""
+    datazine -- a sketchbook and micropublishing platform for sql data
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run()
