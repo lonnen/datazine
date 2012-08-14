d01e20b5660a5896b69c9c397204616d87241720
Lonnen <chris.lonnen@gmail.com>
2012-08-13 19:40:33 -0700

    implement publishing.
    datazine is now effectively a pastebin. also introduces the Foundation
    framework, and some themeing for ace9.

--- datazine.py
+++ datazine.py
@@ -1,14 +1,67 @@
 """
-    datazine -- a sketchbook and micropublishing platform for sql data
+datazine -- a sketchbook and micropublishing platform for sql data
 """
 
-from flask import Flask, render_template
+import settings
+
+from contextlib import closing
+import md5
+
+from flask import abort, Flask, jsonify, redirect, render_template, request, url_for
+import psycopg2
+
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
 
-@app.route("/")
-def hello():
-    return render_template('base.html')
+def connect_datastore():
+    """Returns a new connection to the datastore, where zines are kept"""
+    return psycopg2.connect(
+        host = app.config['DATASTORE_HOST'],
+        port = app.config['DATASTORE_PORT'],
+        dbname = app.config['DATASTORE_NAME'],
+        user = app.config['DATASTORE_NAME'],
+        password = app.config['DATASTORE_PASS']
+    )
+
+
+def init_db():
+    """Creates the database tables.
+
+    intended to be run from a python shell in the root dir:
+        >>> from datazine import init_db; init_db();
+
+    """
+    with closing(connect_datastore()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().execute(f.read())
+        db.commit()
+
+
+@app.route("/publish", methods=["POST"])
+def publish():
+    zine = request.form['zine']
+    title = request.form.get('title', md5.new(zine).hexdigest())
+    with closing(connect_datastore()) as db:
+        db.cursor().execute("INSERT INTO zines VALUES (%s, %s);",
+                            (title, zine))
+        db.commit()
+    return jsonify({'url': url_for('zine', title=title, _external=True)})
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("base.html")
+
+@app.route("/<title>", methods=["GET"])
+def zine(title):
+    with closing(connect_datastore()) as db:
+        try:
+            cur = db.cursor()
+            cur.execute("SELECT * FROM zines where title = %s", (title,))
+            title, zine = cur.fetchone()
+        except TypeError:
+            abort(404)
+    return render_template("base.html", title=title, zine=zine)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=app.config['DEBUG'])
